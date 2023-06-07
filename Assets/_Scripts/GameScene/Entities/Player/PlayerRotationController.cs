d395c049886611c2e4e1604fb1accc7110a620b5
twinkocat <msbulahmaxim@gmail.com>
2023-06-07 21:37:07 +0300

    Fixed player camera Y unbounded rotation.

--- Assets/_Scripts/GameScene/Entities/Player/PlayerRotationController.cs
+++ Assets/_Scripts/GameScene/Entities/Player/PlayerRotationController.cs
@@ -8,6 +8,7 @@ public class PlayerRotationController : MonoBehaviour
     private float                           _playerRotation;
     private static float                    _sensitivityX;
     private static float                    _sensitivityY;
+    private float                           _boundCameraAngle = 85f;
 
     private void Start()
     {
@@ -20,7 +21,8 @@ public class PlayerRotationController : MonoBehaviour
         _cameraRotation += -RotationJoystick.GetVerticalAxis() * _sensitivityY * Time.deltaTime;
 
         transform.rotation = Quaternion.Euler(0f, _playerRotation, 0f);
-        _playerCamera.transform.localRotation = Quaternion.Euler(_cameraRotation, 0f, 0f);
+        _playerCamera.transform.localRotation = Quaternion.Euler(
+                                        Mathf.Clamp(_cameraRotation, -_boundCameraAngle, _boundCameraAngle), 0f, 0f);
     }
 
     public static void SaveSensValues()
